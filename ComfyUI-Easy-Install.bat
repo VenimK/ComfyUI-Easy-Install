a4cfbea6415738ff2e90158730b135214934c225
ivo <36665424+Tavris1@users.noreply.github.com>
2025-02-03 11:57:20 +0200

    Update ComfyUI-Easy-Install.bat

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
-@Echo off
-Title ComfyUI Easy Install by ivo v0.31.0 (Ep31)
+@echo off
+Title ComfyUI Easy Install by ivo v0.31.2 (Ep31)
 :: Pixaroma Community Edition ::
 
 :: Set colors ::
@@ -7,9 +7,9 @@ call :set_colors
 
 :: Check for Existing ComfyUI Folder ::
 if exist ComfyUI_windows_portable (
-	Echo %warning%WARNING:%reset% '%bold%ComfyUI_windows_portable%reset%' folder already exists!
-	Echo %green%Move this file to another folder and run it again.%reset%
-	Echo Press any key to Exit...&Pause>nul
+	echo %warning%WARNING:%reset% '%bold%ComfyUI_windows_portable%reset%' folder already exists!
+	echo %green%Move this file to another folder and run it again.%reset%
+	echo Press any key to Exit...&Pause>nul
 	goto :eof
 )
 
@@ -24,15 +24,13 @@ call :install_7zip
 call :install_git
 call :download_and_install_comfyui
 
-:: Install Main Nodes ::
+:: Install Pixaroma's Related Nodes ::
 call :get_node https://github.com/ltdrdata/ComfyUI-Manager
 call :get_node https://github.com/crystian/ComfyUI-Crystools
 call :get_node https://github.com/rgthree/rgthree-comfy
 call :get_node https://github.com/yolain/ComfyUI-Easy-Use
 call :get_node https://github.com/WASasquatch/was-node-suite-comfyui
 call :get_node https://github.com/city96/ComfyUI-GGUF
-
-:: Pixaroma's Related Nodes ::
 call :get_node https://github.com/MohammadAboulEla/ComfyUI-iTools
 call :get_node https://github.com/Fannovel16/comfyui_controlnet_aux
 call :get_node https://github.com/gseth/ControlAltAI-Nodes
@@ -48,18 +46,11 @@ call :get_node https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite
 call :get_node https://github.com/PowerHouseMan/ComfyUI-AdvancedLivePortrait
 
 :: Install onnxruntime ::
-Echo y|.\python_embeded\python.exe -m pip uninstall onnxruntime-gpu
-.\python_embeded\python.exe -m pip install --no-cache-dir --no-warn-script-location onnxruntime-gpu
-
-:: Install xformers for ControlAltAI Nodes ::
-.\python_embeded\python.exe -m pip install xformers==0.0.29.post1 --index-url https://download.pytorch.org/whl/cu124
-
-:: Erase pip cache ::
-if exist "%localappdata%\pip\cache" rd /s /q "%localappdata%\pip\cache"
+.\python_embeded\python.exe -m pip install onnxruntime-gpu --no-cache-dir --no-warn-script-location
 
 :: Copy additional files if they exist ::
-md ComfyUI\user\default&&call :copy_files comfy.settings.json ComfyUI\user\default
-
+md ComfyUI\user\default
+call :copy_files comfy.settings.json	ComfyUI\user\default
 call :copy_files run_nvidia_gpu.bat		.\
 call :copy_files extra_model_paths.yaml	ComfyUI
 call :copy_files config.ini				ComfyUI\custom_nodes\ComfyUI-Manager
@@ -73,10 +64,10 @@ for /f %%i in ('powershell -command "Get-Date -Format HH:mm:ss"') do set end=%%i
 for /f %%i in ('powershell -command "(New-TimeSpan -Start (Get-Date '%start%') -End (Get-Date '%end%')).TotalSeconds"') do set diff=%%i
 
 :: Completion Message ::
-Echo.
-Echo %green%::::::::::::::: Installation Complete :::::::::::::::%reset%
-Echo %green%::::::::::::::: Total Running Time:%reset%%red% %diff% %reset%%green%seconds%reset%
-Echo %yellow%::::::::::::::: Press any key to exit :::::::::::::::%reset%&Pause>nul
+echo.
+echo %green%::::::::::::::: Installation Complete :::::::::::::::%reset%
+echo %green%::::::::::::::: Total Running Time:%reset%%red% %diff% %reset%%green%seconds%reset%
+echo %yellow%::::::::::::::: Press any key to exit :::::::::::::::%reset%&Pause>nul
 goto :eof
 
 ::::::::::::::::::::::::::::::::: END :::::::::::::::::::::::::::::::::
@@ -92,66 +83,70 @@ goto :eof
 
 :install_7zip
 :: https://www.7-zip.org/
-Echo %green%::::::::::::::: Installing/Updating 7-Zip :::::::::::::::%reset%
-Echo.
+echo %green%::::::::::::::: Installing/Updating 7-Zip :::::::::::::::%reset%
+echo.
 winget install -e --id 7zip.7zip
-Echo.
+echo.
 goto :eof
 
 :install_git
 :: https://git-scm.com/
-Echo %green%::::::::::::::: Installing/Updating Git :::::::::::::::%reset%
-Echo.
+echo %green%::::::::::::::: Installing/Updating Git :::::::::::::::%reset%
+echo.
 winget install --id Git.Git -e --source winget
 for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%A%%B
 echo %path%|find /i "%ProgramFiles%\Git\cmd">nul || setx path "%userpath%;%ProgramFiles%\Git\cmd"
 echo %path%|find /i "%ProgramFiles%\Git\cmd">nul || set path=%userpath%;%ProgramFiles%\Git\cmd
-Echo.
+echo.
 goto :eof
 
 :download_and_install_comfyui
 :: https://github.com/comfyanonymous/ComfyUI
-Echo %green%::::::::::::::: Downloading ComfyUI :::::::::::::::%reset%
-Echo.
+echo %green%::::::::::::::: Downloading ComfyUI :::::::::::::::%reset%
+echo.
 if exist ComfyUI_windows_portable_nvidia.7z erase ComfyUI_windows_portable_nvidia.7z
 curl.exe -OL https://github.com/comfyanonymous/ComfyUI/releases/download/v0.2.3/ComfyUI_windows_portable_nvidia.7z
 
 if not exist ComfyUI_windows_portable_nvidia.7z (
 	cls
-	Echo %warning%WARNING:%reset% Cannot create %yellow%ComfyUI_windows_portable_nvidia.7z%reset%
-	Echo Make sure you are NOT using system folders like %yellow%Program Files, Windows%reset% or system root %yellow%C:\%reset%
-	Echo %green%Move this file to another folder and run it again.%reset%
-	Echo Press any key to Exit...&Pause>nul
+	echo %warning%WARNING:%reset% Cannot create %yellow%ComfyUI_windows_portable_nvidia.7z%reset%
+	echo Make sure you are NOT using system folders like %yellow%Program Files, Windows%reset% or system root %yellow%C:\%reset%
+	echo %green%Move this file to another folder and run it again.%reset%
+	echo Press any key to Exit...&Pause>nul
 	exit /b
 )
 
-Echo %green%::::::::::::::: Extracting ComfyUI :::::::::::::::%reset%
+echo %green%::::::::::::::: Extracting ComfyUI :::::::::::::::%reset%
 "%ProgramFiles%\7-Zip\7z.exe" x ComfyUI_windows_portable_nvidia.7z
 erase ComfyUI_windows_portable_nvidia.7z
-Echo.
-Echo %green%::::::::::::::: Updating ComfyUI :::::::::::::::%reset%
-Echo.
-.\ComfyUI_windows_portable\python_embeded\python.exe -m pip install --no-cache-dir --no-warn-script-location --upgrade pip
+echo.
+echo %green%::::::::::::::: Updating ComfyUI :::::::::::::::%reset%
+echo.
+.\ComfyUI_windows_portable\python_embeded\python.exe -m pip uninstall -y torch torchvision torchaudio
+.\ComfyUI_windows_portable\python_embeded\python.exe -m pip install --upgrade pip --no-cache-dir --no-warn-script-location
 cd .\ComfyUI_windows_portable\python_embeded\
-python -m pip install  --no-cache-dir --no-warn-script-location torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1  --extra-index-url https://download.pytorch.org/whl/cu124
+python -m pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 xformers==0.0.29.post2 --extra-index-url https://download.pytorch.org/whl/cu126 --no-cache-dir --no-warn-script-location
+cd ..\update\
+if exist update_comfyui_and_python_dependencies.bat rename update_comfyui_and_python_dependencies.bat "!DON'T USE THIS! update_comfyui_and_python_dependencies.bat"
+CALL update_comfyui.bat nopause
 cd ..\..\
-Echo.
+echo.
 goto :eof
 
 :get_node
 set git_url=%~1
 for %%x in (%git_url:/= %) do set git_folder=%%x
-Echo %green%::::::::::::::: Installing %git_folder% :::::::::::::::%reset%
-Echo.
+echo %green%::::::::::::::: Installing %git_folder% :::::::::::::::%reset%
+echo.
 if exist .\ComfyUI_windows_portable\ComfyUI cd .\ComfyUI_windows_portable
 git clone %git_url% ComfyUI/custom_nodes/%git_folder%
 if exist .\ComfyUI\custom_nodes\%git_folder%\requirements.txt (
-	.\python_embeded\python.exe -m pip install --no-cache-dir --no-warn-script-location -r .\ComfyUI\custom_nodes\%git_folder%\requirements.txt
+	.\python_embeded\python.exe -m pip install -r .\ComfyUI\custom_nodes\%git_folder%\requirements.txt --no-cache-dir --no-warn-script-location
 )
 if exist .\ComfyUI\custom_nodes\%git_folder%\install.py (
 	.\python_embeded\python.exe .\ComfyUI\custom_nodes\%git_folder%\install.py
 )
-Echo.
+echo.
 goto :eof
 
 :copy_files
